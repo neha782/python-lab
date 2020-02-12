b1ede19537e2def6d726572cf87d50ba720e6b6c
neha782 <60420512+neha782@users.noreply.github.com>
2020-02-12 11:46:40 +0530

    list1

--- list.py
+++ list.py
@@ -1,8 +1,8 @@
 l=[]
 while 1:
 	item =input("enter the item")
-	l=l+[item]
-	n=input("do you want to continue Y/N")
+	l+=[item]
+	n=input("do tri maa ki you want to continue Y/N")
 	if n.lower()=='n':
 		break
 print(l)
